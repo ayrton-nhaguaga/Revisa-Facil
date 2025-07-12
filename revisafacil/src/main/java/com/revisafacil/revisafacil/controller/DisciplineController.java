08d2c3c8c9cfc2c5d9da589def3fd4d5275d3538
ayrton-nhaguaga <ayrtonjuliojaimenhaguaga>
2025-07-12 12:40:13 +0200

    actualizando

---
+++ revisafacil/src/main/java/com/revisafacil/revisafacil/controller/DisciplineController.java
@@ -0,0 +1,77 @@
+package com.revisafacil.revisafacil.controller;
+
+
+import com.revisafacil.revisafacil.dto.DisciplineDTO;
+import com.revisafacil.revisafacil.model.Discipline;
+import com.revisafacil.revisafacil.service.DisciplineService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/disciplines")
+public class DisciplineController {
+
+    @Autowired
+    private DisciplineService disciplineService;
+
+    @PostMapping
+    public ResponseEntity<Discipline> createDiscipline(@RequestBody DisciplineDTO dto){
+        Discipline discipline = disciplineService.createDiscipline(dto);
+        return new ResponseEntity<>(discipline, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Discipline>> getAllDisciplines(){
+        List<Discipline> disciplines = disciplineService.getAllDisciplines();
+        return new ResponseEntity<>(disciplines, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Discipline>> getDisciplineById(@RequestParam ObjectId id){
+        Optional<Discipline> discipline = disciplineService.getDisciplineById(id);
+        return new ResponseEntity<>(discipline, HttpStatus.OK);
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<List<Discipline>> getDisciplineByNameIgnoreCase(@RequestParam String name){
+        List<Discipline> disciplines = disciplineService.getDisciplineByNameIgnoreCase(name);
+        return new ResponseEntity<>(disciplines, HttpStatus.OK);
+    }
+
+    @GetMapping("/user-id")
+    public ResponseEntity<List<Discipline>> getDisciplineByUserId(@RequestParam ObjectId userId){
+        List<Discipline> disciplines = disciplineService.getDisciplineByUserId(userId);
+        return new ResponseEntity<>(disciplines, HttpStatus.OK);
+    }
+
+    @GetMapping("/create-at")
+    public ResponseEntity<List<Discipline>> getDisciplineByDate(@RequestParam LocalDate createAt){
+        List<Discipline> disciplines = disciplineService.getDisciplineByDate(createAt);
+        return new ResponseEntity<>(disciplines, HttpStatus.OK);
+    }
+
+    @PutMapping("/name")
+    public ResponseEntity<List<Discipline>> updateDisciplineByName(@RequestParam String name, @RequestBody DisciplineDTO dto){
+        List<Discipline> updatedList = disciplineService.updateDisciplineByName(name, dto);
+
+        if (updatedList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(updatedList);
+    }
+
+    @DeleteMapping("/name")
+    public ResponseEntity<Void> deleteDiscipline(@RequestParam String name){
+        if (disciplineService.deleteDiscipline(name)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+}
---
+++ revisafacil/src/main/java/com/revisafacil/revisafacil/service/StudySessionService.java
@@ -0,0 +1,75 @@
+package com.revisafacil.revisafacil.service;
+
+
+import com.revisafacil.revisafacil.dto.StudySessionDTO;
+import com.revisafacil.revisafacil.model.StudySession;
+import com.revisafacil.revisafacil.repository.StudySessionRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudySessionService {
+
+    @Autowired
+    private StudySessionRepository studySessionRepository;
+
+    public StudySession createStudySession(StudySessionDTO dto){
+        StudySession studySession = new StudySession();
+        studySession.setId(dto.getId());
+        studySession.setUserId(dto.getUserId());
+        studySession.setTopicId(dto.getTopicId());
+        studySession.setStartTime(dto.getStartTime());
+        studySession.setEndTime(dto.getEndTime());
+        studySession.setDurationMinutes(dto.getDurationMinutes());
+        return studySessionRepository.save(studySession);
+    }
+
+    public List<StudySession> findAll(){
+        return studySessionRepository.findAll();
+    }
+
+    public Optional<StudySession> findById(ObjectId id){
+        return studySessionRepository.findById(id);
+    }
+
+    public List<StudySession> findByUserId(ObjectId userId){
+        return studySessionRepository.findByUserId(userId);
+    }
+
+    public List<StudySession> findByTopicId(ObjectId topicId){
+        return studySessionRepository.findByTopicId(topicId);
+    }
+
+    public List<StudySession> findByDuration(int duration){
+        return studySessionRepository.findByDuration(duration);
+    }
+
+    public List<StudySession> updateStudySession(ObjectId topicId, StudySessionDTO dto){
+        List<StudySession> existes = studySessionRepository.findByTopicId(topicId);
+
+        for (StudySession s : existes){
+            s.setTopicId(dto.getTopicId());
+            s.setDurationMinutes(dto.getDurationMinutes());
+            s.setStartTime(dto.getStartTime());
+            s.setEndTime(dto.getEndTime());
+            studySessionRepository.save(s);
+        }
+        return existes;
+    }
+
+    public boolean deleteStudySession(ObjectId topicId){
+        List<StudySession> studySessions = studySessionRepository.findByTopicId(topicId);
+
+        if (!studySessions.isEmpty()){
+            studySessionRepository.deleteAll(studySessions);
+            return true;
+        }
+        return false;
+    }
+
+
+}
